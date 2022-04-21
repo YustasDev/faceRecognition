1884d47ab00ed194ca3dc54d38a1f3330f43dedc
vitaly.gusev <vitaly.gusev@progforce.com>
2022-04-21 15:55:23 +0300

    Working code 0.3 Recognizing a known person in a group of people

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import time
 import PIL.Image as Image
+from PIL import Image, ImageDraw
 from io import BytesIO
 
 import matplotlib.pylab as plt
@@ -66,35 +67,89 @@ if __name__ == '__main__':
 
     # Load the jpg files into numpy arrays
     j_image = face_recognition.load_image_file("./KNOWN_PEOPLE_FOLDER/J1.jpg")
-    me_image = face_recognition.load_image_file("./V1/V3.jpg")
     k_image = face_recognition.load_image_file("./IMAGE_TO_CHECK/K1.jpg")
-    unknown_image = face_recognition.load_image_file("./V1/V6.jpg")
+    me_image = face_recognition.load_image_file("./V1/V3.jpg")
+    unknown_image = face_recognition.load_image_file("./V1/V2.jpg")
+
+    j_encoding = face_recognition.face_encodings(j_image)[0]
+    k_encoding = face_recognition.face_encodings(k_image)[0]
+    me_encoding = face_recognition.face_encodings(me_image)[0]
+
+    known_face_encodings = [j_encoding, k_encoding, me_encoding]
+    known_face_names = ["Jolie", "Katrin", "Me"]
+
+    # Find all the faces and face encodings in the unknown image
+    face_locations = face_recognition.face_locations(unknown_image)
+    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+    # See http://pillow.readthedocs.io/ for more about PIL/Pillow
+    pil_image = Image.fromarray(unknown_image)
+    # Create a Pillow ImageDraw Draw instance to draw with
+    draw = ImageDraw.Draw(pil_image)
+
+    # Loop through each face found in the unknown image
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+            print(name)
+
+        # Draw a box around the face using the Pillow module
+        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+        # Draw a label with a name below the face
+        text_width, text_height = draw.textsize(name)
+        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+    # Remove the drawing library from memory as per the Pillow docs
+    del draw
+
+    # Display the resulting image
+    pil_image.show()
+
+
+
+
+
 
     # Get the face encodings for each face in each image file
     # Since there could be more than one face in each image, it returns a list of encodings.
     # But since I know each image only has one face, I only care about the first encoding in each image, so I grab index 0.
-    try:
-        j_encoding = face_recognition.face_encodings(j_image)[0]
-        k_encoding = face_recognition.face_encodings(k_image)[0]
-        me_encoding = face_recognition.face_encodings(me_image)[0]
-        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-    except IndexError:
-        print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-        quit()
-
-    known_faces = [
-        j_encoding,
-        k_encoding,
-        me_encoding
-    ]
-
-    # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
-    results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
-
-    print("Is the unknown face a picture of J? {}".format(results[0]))
-    print("Is the unknown face a picture of K? {}".format(results[1]))
-    print("Is the unknown face a picture of Me? {}".format(results[2]))
-    print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
+    # try:
+    #     j_encoding = face_recognition.face_encodings(j_image)[0]
+    #     k_encoding = face_recognition.face_encodings(k_image)[0]
+    #     me_encoding = face_recognition.face_encodings(me_image)[0]
+    #     unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+    # except IndexError:
+    #     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    #     quit()
+    #
+    # known_faces = [
+    #     j_encoding,
+    #     k_encoding,
+    #     me_encoding
+    # ]
+    #
+    # # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
+    # results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+    #
+    # print("Is the unknown face a picture of J? {}".format(results[0]))
+    # print("Is the unknown face a picture of K? {}".format(results[1]))
+    # print("Is the unknown face a picture of Me? {}".format(results[2]))
+    # print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
 
 
     """"
