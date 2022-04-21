4218ca6be957eac6a9fc386fe20de631822d031c
vitaly.gusev <vitaly.gusev@progforce.com>
2022-04-21 19:17:04 +0300

    Working code 0.4 Recognizing a known persons in a group of people in video from web-camera

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-import pathlib
 
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from keras import layers
-import idx2numpy
 import cv2
-import matplotlib
 import matplotlib.pyplot as plt
-import time
-import PIL.Image as Image
 from PIL import Image, ImageDraw
-from io import BytesIO
-
 import matplotlib.pylab as plt
-import tensorflow_hub as hub
-import tensorflow_datasets as tfds
-import tf2onnx
-import onnxruntime as ort
-import subprocess
-import onnx
 import torch
-from torchvision import datasets, transforms
-from torch import nn
-import torch.nn.functional as F
 import face_recognition
 
-
-#CLASSIFIER_URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
 IMAGE_RES = 224
-
-def export_to_onnx(model):
-# https://github.com/onnx/tensorflow-onnx/blob/master/tutorials/keras-resnet50.ipynb
-    spec = (tf.TensorSpec((None, 224, 224, 3), tf.float32, name="input"),)
-    output_path = model.name + ".onnx"
-
-    model_proto, _ = tf2onnx.convert.from_keras(model, input_signature=spec, opset=13, output_path=output_path)
-    output_names = [n.name for n in model_proto.graph.output]
 
 def preprocess(img):
     img = img / 255.
@@ -65,6 +36,10 @@ if __name__ == '__main__':
     #                                capture_output=True)
     #     print(str(count_kernels))
 
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+
+
     # Load the jpg files into numpy arrays
     j_image = face_recognition.load_image_file("./KNOWN_PEOPLE_FOLDER/J1.jpg")
     k_image = face_recognition.load_image_file("./IMAGE_TO_CHECK/K1.jpg")
@@ -78,6 +53,79 @@ if __name__ == '__main__':
     known_face_encodings = [j_encoding, k_encoding, me_encoding]
     known_face_names = ["Jolie", "Katrin", "Me"]
 
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 2
+            right *= 2
+            bottom *= 2
+            left *= 2
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+#=================== only for stationary images ==================================>
+    """
     # Find all the faces and face encodings in the unknown image
     face_locations = face_recognition.face_locations(unknown_image)
     face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
@@ -104,7 +152,8 @@ if __name__ == '__main__':
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
-            print(name)
+            print("Found {} face(s) in this picture".format(len(face_locations)))
+            print('Index №' + str(best_match_index) + '  -  this is  ' + name)
 
         # Draw a box around the face using the Pillow module
         draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
@@ -119,172 +168,7 @@ if __name__ == '__main__':
 
     # Display the resulting image
     pil_image.show()
-
-
-
-
-
-
-    # Get the face encodings for each face in each image file
-    # Since there could be more than one face in each image, it returns a list of encodings.
-    # But since I know each image only has one face, I only care about the first encoding in each image, so I grab index 0.
-    # try:
-    #     j_encoding = face_recognition.face_encodings(j_image)[0]
-    #     k_encoding = face_recognition.face_encodings(k_image)[0]
-    #     me_encoding = face_recognition.face_encodings(me_image)[0]
-    #     unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-    # except IndexError:
-    #     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
-    #     quit()
-    #
-    # known_faces = [
-    #     j_encoding,
-    #     k_encoding,
-    #     me_encoding
-    # ]
-    #
-    # # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
-    # results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
-    #
-    # print("Is the unknown face a picture of J? {}".format(results[0]))
-    # print("Is the unknown face a picture of K? {}".format(results[1]))
-    # print("Is the unknown face a picture of Me? {}".format(results[2]))
-    # print("Is the unknown face a new person that we've never seen before? {}".format(not True in results))
-
-
-    """"
-    pictureJ = face_recognition.load_image_file("/home/progforce/facerecognition/V1/V3.jpg")
-    my_face_encoding = face_recognition.face_encodings(pictureJ)[0]
-
-    # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
-
-    unknown_picture = face_recognition.load_image_file("/home/progforce/facerecognition/V1/V10.jpg")
-    unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
-
-    # Now we can see the two face encodings are of the same person with `compare_faces`!
-
-    results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
-
-    if results[0] == True:
-        print("It's the same person")
-    else:
-        print("it's NOT the same person")
     """
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """
-    layer = hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3), trainable=True)
-    #import pdb; pdb.set_trace()
-    model = tf.keras.Sequential([
-        layer
-    ])
-
-    grace_hopper = tf.keras.utils.get_file('image.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
-    #grace_hopper = Image.open('car3.jpg').resize((IMAGE_RES, IMAGE_RES))
-    grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
-
-    # If you need to see a picture
-    # grace_hopper.save("grace.jpg")
-    # picture = cv2.imread('grace.jpg')
-    # cv2.imshow('grace_hopper', picture)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    grace_hopper_div = np.array(grace_hopper) / 255.0
-    #print(np.shape(grace_hopper_div))
-    #print(np.shape(grace_hopper_div[np.newaxis, ...]))   # models always want a batch of images to process. So here, we add a batch dimension, to pass the image to the model for prediction.
-
-
-    result = model.predict(grace_hopper_div[np.newaxis, ...])
-    print(result.shape)
-
-    predicted_class = np.argmax(result[0], axis=-1)
-
-
-    labels_path = tf.keras.utils.get_file('ImageNetLabels.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
-    imagenet_labels = np.array(open(labels_path).read().splitlines())
-
-    predicted_class_name = imagenet_labels[predicted_class]
-    title = predicted_class_name.title()
-    print('Data from TF-model ...  there must be military_uniform')
-    print(predicted_class, predicted_class_name)
-
-    # grace_hopper.save("grace.jpg")
-    # picture = cv2.imread('grace.jpg')
-    # cv2.imshow(title, picture)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-#================================ save model as ONNX and launch it ==========================================>
-    proc = subprocess.run('python -m tf2onnx.convert --opset 13 --saved-model "home/progforce/TestModels" --output pf1.onnx'.split(),
-                          capture_output=True)   # it creates  sequential.onnx   why?????
-
-    onnx_model = onnx.load('sequential.onnx')
-    session = ort.InferenceSession(onnx_model.SerializeToString())
-
-    pathImg = pathlib.Path('cat2.jpg')
-    img = get_image(pathImg)
-    img = preprocess(img)
-    ort_input = {session.get_inputs()[0].name: img}
-    preds = session.run(None, ort_input)[0]
-
-    print(preds.shape)
-    onnx_predicted_class = np.argmax(preds)
-    onnx_predicted_class_name = imagenet_labels[onnx_predicted_class]
-    print('Data from ONNX-model... there must be a cat :)')
-    print(onnx_predicted_class, onnx_predicted_class_name)
-
-
-    #
-    # for number in range (1000):
-    #     class_name = imagenet_labels[number]
-    #     title = class_name.title()
-    #     print(number, ' ', title)
-
-
-#===================================== full preservation of TF model ===============================>
-    model.save('/home/progforce/TestModels/')
-    restore_model = keras.models.load_model('/home/progforce/TestModels/')
-    restore_model.summary()
-
-#===================================== save and load model as Keras .h5 ============================>
-
-    # t = time.time()
-    # export_path_keras = "./{}.h5".format(int(t))
-    model.save('myKeras.h5', save_format='h5')
-
-    # `custom_objects` tells keras how to load a `hub.KerasLayer`
-    reloaded_model = keras.models.load_model('myKeras.h5', custom_objects={'KerasLayer': hub.KerasLayer})
-    print("It's from h5_model")
-    reloaded_model.summary()
-
-    print('Data from reloaded model.. it should be the same cat')
-    predict_from_reload = reloaded_model.predict(img)
-    predicted_class_from_reload = np.argmax(predict_from_reload[0], axis=-1)
-    predicted_class_name_from_reload = imagenet_labels[predicted_class_from_reload]
-    print(predicted_class_from_reload, predicted_class_name_from_reload)
-
-
-
-    #====================== Display image ================================>
-    # img = cv2.imread('/home/progforce/Pictures/inJava.jpg')
-    # cv2.imshow('Java', img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    """
